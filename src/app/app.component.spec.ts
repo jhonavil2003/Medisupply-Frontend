18dbfb463b05b6cf7c26477ce6b1e28d73e02228
fquinteroc <f.quinteroc@uniandes.edu.co>
2025-10-22 18:34:06 -0500

    Actualizar tests para arquitectura standalone

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { provideRouter } from '@angular/router';
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient } from '@angular/common/http';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 describe('AppComponent', () => {
   let component: AppComponent;
@@ -10,13 +11,16 @@ describe('AppComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-    imports: [
-        AppRoutingModule,
-        HttpClientModule,
-        AppComponent
-    ],
-}).compileComponents();
+      imports: [AppComponent],
+      providers: [
+        provideRouter([]),
+        provideHttpClient(),
+        provideAnimations(),
+        provideToastr()
+      ]
+    }).compileComponents();
   });
+
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
